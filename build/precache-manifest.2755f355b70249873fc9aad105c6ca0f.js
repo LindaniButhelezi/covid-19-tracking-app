e3b0d15a8d519e3727170d17e25e886210f285ed
LindaniButhelezi <64778101+LindaniButhelezi@users.noreply.github.com>
2020-05-08 17:38:07 +0200

    build

---
+++ build/precache-manifest.2755f355b70249873fc9aad105c6ca0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af7c4baf528209d1d8e4ab95c4fcb6d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71d1dede8d73dba7fe36",
+    "url": "/static/css/main.d29e8798.chunk.css"
+  },
+  {
+    "revision": "5bd58af8c5b3cc196593",
+    "url": "/static/js/2.de2448e8.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.de2448e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71d1dede8d73dba7fe36",
+    "url": "/static/js/main.3668ba75.chunk.js"
+  },
+  {
+    "revision": "26091c8ef446eebb46f6",
+    "url": "/static/js/runtime-main.86d960fd.js"
+  },
+  {
+    "revision": "85fa7dd66d033d0d965bd9a1f873eef3",
+    "url": "/static/media/image.85fa7dd6.png"
+  }
+]);
